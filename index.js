3d0369320f790235a98d6a89446eb6889a67e565
CaterCode <catercode00@gmail.com>
2021-12-23 23:15:22 +0530

    make it better

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import myTodoModel from './models/todo_schema.js';
 
 dotenv.config();
@@ -10,106 +11,88 @@ const port = process.env.PORT || 1000;
 const DB = process.env.DB_URL_DB;
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json());
+
 
 //this is our first home route
 app.get('/todo', async (req, res) => {
-    const getAllTodo = await myTodoModel.find({})
-    if (getAllTodo) {
-        return res.status(200).json({
-            status: true,
-            message: 'Todo load successful',
-            data: getAllTodo
-        })
-    } else {
-        return res.status(404).json({
-            status: false,
-            message: 'Failed to load todo',
-
-        })
+    try {
+        const getAllTodo = await myTodoModel.find({})
+        if (getAllTodo) {
+            res.send(getAllTodo)
+        } else {
+            res.send(getAllTodo)
+        }
+    } catch (error) {
+      console.log(error)
     }
+
 })
 app.get('/todo/:completed', async (req, res) => {
-    const { completed } = req.params
-    console.log(completed)
-    const updateTodo = await myTodoModel.find({}).where('completed').equals(completed)
-    if (updateTodo) {
-        return res.status(200).json({
-            status: true,
-            message: 'Todo fatch successful',
-            data: updateTodo
-        })
+    try {
+        const { completed } = req.params
+        const updateTodo = await myTodoModel.find({}).where('completed').equals(completed)
+        if (updateTodo) {
+            res.send(updateTodo)
+        } else {
+            res.send(updateTodo)
+        }
 
-    } else {
-        return res.status(200).json({
-            status: false,
-            message: 'Failed to fatch todo',
-            data: updateTodo
-        })
+    } catch (error) {
+        console.log(error)
     }
-
+    
 })
 
 //this is our todo route
 app.post('/todo', async (req, res) => {
-    const { title, description, dateTime, completed } = req.body
-    const addNewTodo = await myTodoModel.create({
-        title,
-        description,
-        dateTime,
-        completed
-    })
-    if (addNewTodo) {
-        return res.status(200).json({
-            status: true,
-            message: 'Todo add successful',
-            data: addNewTodo
+    try {
+        const { title, description, dateTime, completed } = req.body
+        const addNewTodo = await myTodoModel.create({
+            title,
+            description,
+            dateTime,
+            completed
         })
-    } else {
-        return res.status(404).json({
-            status: false,
-            message: 'Todo add failed',
-
-        })
+        if (addNewTodo) {
+            res.send(addNewTodo)
+        } else {
+            res.send(addNewTodo)
+        }
+    } catch (error) {
+        console.log(error)
     }
+   
 
 })
 
 //this is my update route
 app.put('/todo/:id', async (req, res) => {
-    const { completed } = req.body
-    const { id } = req.params.id
-    const updateTodo = await myTodoModel.findByIdAndUpdate(req.params.id, req.body)
-    if (updateTodo) {
-        return res.status(200).json({
-            status: true,
-            message: 'Todo update successful',
-            data: updateTodo
-        })
-    } else {
-        return res.status(404).json({
-            status: false,
-            message: 'Todo update failed',
-        })
+   try {
+       const updateTodo = await myTodoModel.findByIdAndUpdate(req.params.id, req.body)
+       if (updateTodo) {
+           res.send(updateTodo)
+       } else {
+           res.send(updateTodo)
 
-    }
+       }
+   } catch (error) {
+       console.log(error)
+   }
 
 })
 //this is my delete route
 app.delete('/todo/:id', async (req, res) => {
-    const deleteTodo = await myTodoModel.findByIdAndDelete(req.params.id)
-    if (deleteTodo) {
-        return res.status(200).json({
-            status: true,
-            message: 'Todo deleted successsful',
-            data: deleteTodo
-        })
-    } else {
-        return res.status(404).json({
-            status: false,
-            message: 'Todo deleted failed',
-            data: deleteTodo
-        })
-    }
+   try {
+       const deleteTodo = await myTodoModel.findByIdAndDelete(req.params.id)
+       if (deleteTodo) {
+           res.send(deleteTodo)
+       } else {
+           res.send(deleteTodo)
+       }
+   } catch (error) {
+       console.log(error)
+   }
 })
 
 mongoose.connect(DB, {
